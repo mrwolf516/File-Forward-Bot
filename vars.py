30fb0a08079d2225c02e7e84d6b473742618a0dc
mrwolf516 <161583562+mrwolf516@users.noreply.github.com>
2024-03-03 13:11:59 +0530

    Update vars.py

--- vars.py
+++ vars.py
@@ -18,4 +18,4 @@ LOG_CHANNEL = int(environ.get("LOG_CHANNEL", 0))
 PORT = int(environ.get("PORT", "8080"))
 ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '').split()]
 TARGET_DB = int(environ.get("TARGET_DB", 0))
-UPSTREAM_REPO = environ.get("UPSTREAM_REPO", "https://github.com/Joelkb/File-Forward-Bot")
+UPSTREAM_REPO = environ.get("UPSTREAM_REPO", "https://github.com/mrwolf516/File-Forward-Bot")
