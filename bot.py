fbaecb0c4fbc751e10c84d9480c23e198432714b
mrwolf516 <161583562+mrwolf516@users.noreply.github.com>
2024-03-03 14:21:03 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,16 +1,6 @@
 from pyrogram import Client, __version__
 from pyrogram.raw.all import layer
-from datetime import date, datetime
-from vars import SESSION, API_HASH, API_ID, BOT_TOKEN, LOG_CHANNEL, PORT
-from typing import Union, Optional, AsyncGenerator
-from script import scripts
-from pyrogram import types
-from utils import temp_utils
-from aiohttp import web
-from plugins import web_server
-import logging
-import pytz
-import logging.config
+
 
 #Get logging configuration
 logging.config.fileConfig('logging.conf')
